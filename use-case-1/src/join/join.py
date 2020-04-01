e7ca269b7d4074445c614cb0001dd6afe2af4c83
Alexandre Delarue <aldelar@microsoft.com>
2020-04-01 03:04:34 +0000

    full dataprep code

--- use-case-1/src/join/join.py
+++ use-case-1/src/join/join.py
@@ -16,11 +16,8 @@ print("Input 2: %s" % args.input_2)
 print("Output : %s" % args.output)
 
 # Retrieve Input Dataset
-run_context = Run.get_context()
-input_ds_1 = run_context.input_datasets[args.input_1]
-input_ds_2 = run_context.input_datasets[args.input_2]
-input_df_1 = input_ds_1.to_pandas_dataframe()
-input_df_2 = input_ds_2.to_pandas_dataframe()
+input_df_1 = pd.read_csv(args.input_1)
+input_df_2 = pd.read_csv(args.input_2)
 
 # Join
 output_df = pd.merge(input_df_1,input_df_2,on='DATE',how='inner')
