4b3cba97fcaaadeb1be692a9b960af4ba93827c6
Alexandre Delarue <aldelar@microsoft.com>
2020-04-01 23:18:21 +0000

    integrating 3rd time-series

--- use-case-1/src/join/join.py
+++ use-case-1/src/join/join.py
@@ -1,4 +1,8 @@
 #
+# join: joins 2 or 3 datasets on the same join_column (set input_3 to '' if only 2 datasets to join)
+#
+
+#
 import argparse, os
 import pandas as pd
 
@@ -7,19 +11,25 @@ parser = argparse.ArgumentParser("join")
 parser.add_argument("--join_column", type=str, help="join_column")
 parser.add_argument("--input_1", type=str, help="input_1")
 parser.add_argument("--input_2", type=str, help="input_2")
+parser.add_argument("--input_3", type=str, help="input_3")
 parser.add_argument("--output", type=str, help="output")
 args = parser.parse_args()
 print("Join Column: %s" % args.join_column)
 print("Input 1: %s" % args.input_1)
 print("Input 2: %s" % args.input_2)
+print("Input 3: %s" % args.input_3)
 print("Output : %s" % args.output)
 
-# Retrieve Input Dataset
+# Retrieve Input Datasets
 input_df_1 = pd.read_csv(args.input_1)
 input_df_2 = pd.read_csv(args.input_2)
+if args.input_3:
+    input_df_3 = pd.read_csv(args.input_3)
 
-# Join
-output_df = pd.merge(input_df_1,input_df_2,on=args.join_column,how='inner',suffixes=('_L', '_R'))
+# Join Datasets
+output_df = input_df_1.merge(input_df_2,on=args.join_column,how='inner',suffixes=('_L1', '_R1'))
+if args.input_3:
+    output_df = output_df.merge(input_df_3,on=args.join_column,how='inner',suffixes=('_L2', '_R2'))
 
 # write output dataset
 output_df.to_csv(args.output,index=False,encoding='utf-8')
--- use-case-1/src/pivot/pivot.py
+++ use-case-1/src/pivot/pivot.py
@@ -1,4 +1,9 @@
 #
+# pivot: normalizes DATE + HOUR columns as DATETIME COLUMN
+#        and optionally pivots data if pivot_columns are provided
+#
+
+#
 from azureml.core import Run, Dataset
 
 #
@@ -13,8 +18,18 @@ def gen_date(d,h):
 
 # Script Parameters
 parser = argparse.ArgumentParser("pivot")
+parser.add_argument("--date_column", type=str, help="date_column")
+parser.add_argument("--hour_column", type=str, help="hour_column")
+parser.add_argument("--datetime_column_name", type=str, help="datetime_column_name")
+parser.add_argument("--pivot_columns", type=str, help="pivot_columns")
+parser.add_argument("--value_column", type=str, help="value_column")
 parser.add_argument("--output", type=str, help="output")
 args = parser.parse_args()
+print("Date Column: %s" % args.date_column)
+print("Hour Column: %s" % args.hour_column)
+print("Datetime Column Name: %s" % args.datetime_column_name)
+print("Pivot Columns: %s" % args.pivot_columns)
+print("Value Column: %s" % args.value_column)
 print("Output: %s" % args.output)
 
 # Retrieve Input Dataset
@@ -25,12 +40,17 @@ input_df = input_ds.to_pandas_dataframe()
 # NOTE: Ability to develop/work on samples from the original dataset (0.01 = 1% of full dataset)
 # input_df = input_dataset.take_sample(0.01).to_pandas_dataframe()
 
-# Generate timestamp column 'DATE' FROM 'MYDATE' and 'HOUR'
-input_df['DATE'] = input_df.apply(lambda x: gen_date(x.MYDATE, x.HOUR), axis=1)
-# Only keep necessary columns
-input_df = input_df[['DATE','NODE_ID','MW']]
+# Generate timestamp column 'DATETIME' FROM date AND hour columns
+input_df[args.datetime_column_name] = input_df.apply(lambda x: gen_date(x[args.date_column], x[args.hour_column]), axis=1)
+# Drop date AND hour columns
+input_df = input_df.drop(columns=[args.date_column,args.hour_column])
 # Pivot Data
-output_df = pd.pivot_table(input_df, values='MW', index='DATE', columns='NODE_ID', aggfunc=np.max)
+if args.pivot_columns:
+    # pivot and set index to datetime
+    output_df = pd.pivot_table(input_df, values=args.value_column, index=args.datetime_column_name, columns=args.pivot_columns, aggfunc=np.max)
+else:
+    # set index to datetime
+    output_df = input_df.set_index(args.datetime_column_name)
 
 # write output dataset
 output_df.to_csv(args.output,encoding='utf-8')
