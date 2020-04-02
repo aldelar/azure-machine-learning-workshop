acf28d2a672f0180de1c9a6a711972b89623e557
Alexandre Delarue <aldelar@microsoft.com>
2020-04-02 18:22:15 +0000

    join with cleanup date feature

--- use-case-1/src/groupby-aggregate/groupby-aggregate.py
+++ use-case-1/src/groupby-aggregate/groupby-aggregate.py
@@ -2,9 +2,6 @@
 # grouby-aggregate: groups by date and generate aggregates for each value column
 #
 
-#
-#from azureml.core import Run, Dataset
-#
 import argparse, os
 import pandas as pd
 import numpy as np
@@ -14,20 +11,16 @@ import dateutil
 parser = argparse.ArgumentParser("groupby-aggregate")
 parser.add_argument("--datetime_column", type=str, help="datetime_column")
 parser.add_argument("--date_column_name", type=str, help="date_column_name")
-parser.add_argument("--input_ds", type=str, help="input_ds")
-parser.add_argument("--output_ds", type=str, help="output_ds")
+parser.add_argument("--input_pd", type=str, help="input_pd")
+parser.add_argument("--output_pd", type=str, help="output_pd")
 args = parser.parse_args()
 print("Datetime Column: %s" % args.datetime_column)
 print("Date Column Name: %s" % args.date_column_name)
-print("Input : %s" % args.input_ds)
-print("Output : %s" % args.output_ds)
+print("Input : %s" % args.input_pd)
+print("Output : %s" % args.output_pd)
 
-# Retrieve Input Datasets
-# Get data from inputs, which are pre-mounted file paths
-#input_ds = Run.get_context().input_datasets["input"]
-#input_df = input_ds.to_pandas_dataframe()
-
-input_df = pd.read_csv(args.input_ds)
+# Retrieve input dataset
+input_df = pd.read_csv(args.input_pd)
 
 # Generate DATE column
 input_df[args.date_column_name] = input_df.apply(lambda x: dateutil.parser.parse(x[args.datetime_column]).date(), axis=1)
@@ -35,7 +28,8 @@ input_df[args.date_column_name] = input_df.apply(lambda x: dateutil.parser.parse
 # Aggregates
 output_df = input_df.groupby(args.date_column_name).aggregate([min, max, sum, np.median, np.mean, np.std])
 output_df.columns = output_df.columns.to_series().str.join('_')
+output_df[args.date_column_name] = output_df.index
 
 # write output dataset
-output_df.to_csv(args.output_ds,index=False,encoding='utf-8')
-print("%s created" % args.output)
+output_df.to_csv(args.output_pd,index=False,encoding='utf-8')
+print("%s created" % args.output_pd)
--- use-case-1/src/join/join.py
+++ use-case-1/src/join/join.py
@@ -5,6 +5,7 @@
 #
 import argparse, os
 import pandas as pd
+from dateutil.parser import parse
 
 # Script Parameters
 parser = argparse.ArgumentParser("join")
@@ -13,12 +14,14 @@ parser.add_argument("--input_1", type=str, help="input_1")
 parser.add_argument("--input_2", type=str, help="input_2")
 parser.add_argument("--input_3", type=str, help="input_3")
 parser.add_argument("--output", type=str, help="output")
+parser.add_argument("--cleanup_date_column", type=str, help="cleanup_date_column")
 args = parser.parse_args()
 print("Join Column: %s" % args.join_column)
 print("Input 1: %s" % args.input_1)
 print("Input 2: %s" % args.input_2)
 print("Input 3: %s" % args.input_3)
 print("Output : %s" % args.output)
+print("Cleanup Date Column : %s" % args.cleanup_date_column)
 
 # Retrieve Input Datasets
 # Get data from inputs, which are pre-mounted file paths
@@ -31,6 +34,10 @@ if args.input_3:
 output_df = input_df_1.merge(input_df_2,on=args.join_column,how='inner',suffixes=('_L1', '_R1'))
 if args.input_3:
     output_df = output_df.merge(input_df_3,on=args.join_column,how='inner',suffixes=('_L2', '_R2'))
+
+# parse date to cleanup to standard format
+if args.cleanup_date_column:
+    output_df[args.cleanup_date_column].apply(lambda x: parse(x[args.cleanup_date_column]).date(), axis=1)
 
 # write output dataset
 output_df.to_csv(args.output,index=False,encoding='utf-8')
